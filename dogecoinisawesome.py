cbd3f18cfd4a5f7b44025a8931f7a97d6929d39f
Jacek Trociński <jack.trocinski@gmail.com>
2022-03-19 10:47:57 +0100

    Change formatting.

--- dogecoinisawesome.py
+++ dogecoinisawesome.py
@@ -1,4 +1,10 @@
 from app import app, port
 
 if __name__ == "__main__":
-    app.run(use_debugger=False, use_reloader=False, passthrough_errors=True, host='0.0.0.0', port=port)
+    app.run(
+        use_debugger=False,
+        use_reloader=False,
+        passthrough_errors=True,
+        host="0.0.0.0",
+        port=port,
+    )
