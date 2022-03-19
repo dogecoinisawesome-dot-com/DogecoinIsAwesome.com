4fc6d3ff8cdeb801bf8db854bbe064b2a7887940
Jacek Trociński <jack.trocinski@gmail.com>
2022-03-19 11:03:04 +0100

    Reorganize routes.

--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,9 @@ from flask import abort, render_template
 
 from app import app
 
-
+#
+# Main pages.
+#
 @app.route("/")
 @app.route("/index")
 def homepage():
@@ -13,10 +15,18 @@ def homepage():
         abort(404)
 
 
-@app.route("/full-node")
-def full_node():
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+
+#
+# Guides.
+#
+@app.route("/dogecoin-wallets")
+def wallets():
     try:
-        readme_file = open("./app/static/docs/full-node.md", "r")
+        readme_file = open("./app/static/docs/dogecoin-wallets.md", "r")
         md_template_string = markdown.markdown(
             readme_file.read(),
             extensions=[
@@ -27,7 +37,7 @@ def full_node():
         )
         return render_template(
             "content.html",
-            title="Running a Full Node",
+            title="Which wallet is best for Dogecoin?",
             markdown_html=md_template_string,
         )
     except IndexError:
@@ -55,10 +65,10 @@ def faq():
         abort(404)
 
 
-@app.route("/dogecoin-wallets")
-def wallets():
+@app.route("/full-node")
+def full_node():
     try:
-        readme_file = open("./app/static/docs/dogecoin-wallets.md", "r")
+        readme_file = open("./app/static/docs/full-node.md", "r")
         md_template_string = markdown.markdown(
             readme_file.read(),
             extensions=[
@@ -69,13 +79,8 @@ def wallets():
         )
         return render_template(
             "content.html",
-            title="Which wallet is best for Dogecoin?",
+            title="Running a Full Node",
             markdown_html=md_template_string,
         )
     except IndexError:
         abort(404)
-
-
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template("404.html"), 404
